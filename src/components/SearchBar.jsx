f633018d1f12466085e279f7d74ed177f3300cab
Sourav Majumdar <souravmajumdar.developer@gmail.com>
2020-12-09 07:38:31 +0530

    Changing to Serach component class to funcation

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,26 @@
-import React, { Component } from 'react';
-
-export class SearchBar extends Component {
-  state = { term: '' };
-
-  onInputChanged = (e) => {
-    this.setState({ term: e.target.value });
+import React, { useState } from 'react';
+const SearchBar = ({ onFormSubmit }) => {
+  const [term, setTerm] = useState('');
+  const onInputChanged = (e) => {
+    setTerm(e.target.value);
   };
 
-  onFormSubmit = (e) => {
+  const onSubmit = (e) => {
     e.preventDefault();
-    this.props.onFormSubmit(this.state.term);
+    onFormSubmit(term);
   };
 
-  render() {
-    return (
-      <div className='search-bar ui segment'>
-        <form onSubmit={this.onFormSubmit} className='ui form'>
-          <div className='field'>
-            <label>Video Search</label>
-            <input type='text' value={this.state.term} onChange={this.onInputChanged} />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+  return (
+    <div className='search-bar ui segment'>
+      <form onSubmit={onSubmit} className='ui form'>
+        <div className='field'>
+          <label>Video Search</label>
+          <input type='text' value={term} onChange={onInputChanged} />
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default SearchBar;
 // AIzaSyDl_DruuOusutTFXwuJPBlgiHLgdPE5JsM;
